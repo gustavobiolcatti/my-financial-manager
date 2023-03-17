89fc7ef90a5b9757ef960af5f8f0620922c2884c
Gustavo Biolcatti <gustavobiolcatti@hotmail.com>
2023-03-17 16:51:23 -0300

    feat: trocar svg por icones ro react-icons

--- src/components/atoms/ActionButton/styles.ts
+++ src/components/atoms/ActionButton/styles.ts
@@ -1,24 +1,13 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import { ActionButtonProps } from './types';
 
 export const ActionButton = styled.button<ActionButtonProps>`
-  ${({ actionType }) => css`
-    width: 25px;
-    height: 25px;
+  margin-left: 1em;
 
-    margin-left: 0.5em;
+  transition: 0.2s;
 
-    background: ${actionType === 'delete'
-      ? 'url(src/assets/img/trash.svg)'
-      : 'url(src/assets/img/pencil.svg)'};
-    background-repeat: no-repeat;
-    background-size: cover;
-
-    transition: 0.2s;
-
-    :hover {
-      transform: scale(0.9);
-    }
-  `}
+  :hover {
+    transform: scale(0.9);
+  }
 `;
